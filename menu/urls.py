7429b38b7933edbf614802731c7463a789c355ee
nimakkhezri <nimakoohkhezri@gmail.com>
2025-08-17 21:29:03 +0330

    feat: add template views

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import menu_view
+from .views import index_view, menu_view, about_view
 
+app_name = "menu_app"
 
 urlpatterns = [
-    path('', menu_view, name='menu_view'),
+    path('', index_view, name='index'),
+    path('menu/', menu_view, name='menu'),
+    path('about/', about_view, name='about'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Category
 
+def index_view(request):
+    return render(request, 'index.html')
+
 def menu_view(request):
     categories = Category.objects.prefetch_related('products__types').all()
     return render(request, 'menu.html', {'categories': categories})
+
+def about_view(request):
+    return render(request, 'about.html')
